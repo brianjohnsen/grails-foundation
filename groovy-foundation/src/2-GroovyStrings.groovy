7f8b872113eb98122d8cf811843afa4554aae020
Brian Johnsen <github@brianjohnsen.dk>
2014-03-17 20:21:23 +0100

    Groovy Strings example

---
+++ groovy-foundation/src/2-GroovyStrings.groovy
@@ -0,0 +1,11 @@
+assert "${'Groovy'} rules!" == 'Groovy rules!'
+
+def eight = 8
+def years = "sixth"
+println "GR$eight times to be had at GR${eight}Conf. This year for the ${years} time in a roll"
+//println "The 5th letter in 'Encyclopedia' is ​${'Encyclopedia'[4]}​"
+
+
+assert "${1337/100 * Math.PI as Integer}" == '42'
+
+
